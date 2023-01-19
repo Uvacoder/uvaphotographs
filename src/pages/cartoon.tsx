e9d27aeb0362fc502957dab666fe72c7a8efbb8e
uvacoder <uva22940coder@gmail.com>
2023-01-19 11:40:59 -0500

    Update and rename people.tsx to cartoon.tsx

--- src/pages/cartoon.tsx
+++ src/pages/cartoon.tsx
@@ -2,12 +2,12 @@ import Footer from "../components/Footer";
 import Header from "../components/Header";
 import ImageGrid from "../components/ImageGrid";
 
-export default function People() {
+export default function Cartoon() {
   return (
     <div className="grid grid-rows-[auto_auto_auto] h-full px-4 pt-4 pb-2 md:px-8 md:pt-6 md:pb-4">
-      <Header title="People" active={3} />
+      <Header title="Cartoon" active={3} />
       <div className="w-full overflow-y-auto">
-        <ImageGrid folder="people" images={151} />
+        <ImageGrid folder="cartoon" images={151} />
       </div>
       <Footer />
     </div>
