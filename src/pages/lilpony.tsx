4f5247b157a6feff8d0949a6aad7043c4d93763b
uvacoder <uva22940coder@gmail.com>
2023-01-19 11:42:00 -0500

    Update and rename show.tsx to lilpony.tsx

--- src/pages/lilpony.tsx
+++ src/pages/lilpony.tsx
@@ -2,12 +2,12 @@ import Footer from "../components/Footer";
 import Header from "../components/Header";
 import ImageGrid from "../components/ImageGrid";
 
-export default function Show() {
+export default function Lilpony() {
   return (
     <div className="grid grid-rows-[auto_auto_auto] h-full px-4 pt-4 pb-2 md:px-8 md:pt-6 md:pb-4">
-      <Header title="Show" active={4} />
+      <Header title="Lilpony" active={4} />
       <div className="w-full overflow-y-auto">
-        <ImageGrid folder="show" images={166} />
+        <ImageGrid folder="lilpony" images={166} />
       </div>
       <Footer />
     </div>
